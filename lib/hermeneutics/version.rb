d2eda863142261068da2dbf74da298429a942af4
Bertram Scharpf <software@bertram-scharpf.de>
2023-08-03 21:13:11 +0200

    Version bump

--- lib/hermeneutics/version.rb
+++ lib/hermeneutics/version.rb
@@ -13,7 +13,7 @@
 module Hermeneutics
 
   NAME        = "hermeneutics"
-  VERSION     = "1.17".freeze
+  VERSION     = "1.18".freeze
   SUMMARY     = "CGI and mail handling"
 
   DESCRIPTION = <<~EOT
