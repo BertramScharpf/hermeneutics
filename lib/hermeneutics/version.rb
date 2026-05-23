8be11b002857f52395a3bcbd196f3a95599ca3c2
Bertram Scharpf <software@bertram-scharpf.de>
2026-05-23 22:59:32 +0200

    Version number

--- lib/hermeneutics/version.rb
+++ lib/hermeneutics/version.rb
@@ -13,7 +13,7 @@
 module Hermeneutics
 
   NAME        = "hermeneutics"
-  VERSION     = "1.23".freeze
+  VERSION     = "1.24".freeze
   SUMMARY     = "CGI and mail handling"
 
   DESCRIPTION = <<~EOT
